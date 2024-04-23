67be39fcc9bc48b0078a54a56aa224d32bdb5380
M Faruk <MohamedFaruk.MohamedYasin@tranetechnologies.com>
2024-04-23 23:57:50 +0530

    init vagrant config

---
+++ vagrant/docker-compose.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -eu
+#
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+export DEBIAN_FRONTEND=noninteractive
+
+# ----------------------------------------------------------------
+# Install docker-compose
+# ----------------------------------------------------------------
+apt install docker-compose
